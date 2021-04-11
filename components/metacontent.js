e1897ee8ba3fb280091166ec25aa8a186341c64a
Thomas Sankara <thomassankara74@gmail.com>
2021-04-11 12:26:57 +0300

    meta tags

---
+++ components/metacontent.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const metacontent = () => {
+  return (
+    <>
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="IE=7" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="description" content="Thomas Sankara Portfolio Website" />
+      <meta
+        name="keywords"
+        content="Thomas Sankara, TsbSankara, TsbDesigns, Thomas Sankara Web Developer, TSB Web Development, SankThomas"
+      />
+    </>
+  )
+}
+
+export default metacontent
