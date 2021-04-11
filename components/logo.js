aa6e5f716cc711330db6b2b4917b1ea46d8b4ede
Thomas Sankara <thomassankara74@gmail.com>
2021-04-11 12:26:23 +0300

    Logo component

---
+++ components/logo.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import React from "react"
+
+const logo = () => {
+  return (
+    <>
+      <div>
+        <Link href="/">
+          <a className="text-4xl uppercase font-bold">Sankara</a>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default logo
