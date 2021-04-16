76509dc9378979a6d4912bbdad01b23aa5a57fa4
Thomas Sankara <thomassankara74@gmail.com>
2021-04-16 22:40:00 +0300

    courses data array

---
+++ data/courses.js
@@ -0,0 +1,69 @@
+import { v4 as uuidv4 } from "uuid"
+// url = Demo button
+export const challenges = [
+  {
+    id: uuidv4(),
+    title: "Blogr Landing Page",
+    // url: "tsbdesigns-blogr.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Bookmark Landing Page",
+    // url:""
+  },
+  {
+    id: uuidv4(),
+    title: "Clipboard Landing Page",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Easybank Landing Page",
+    // url:""
+  },
+  {
+    id: uuidvv4(),
+    title: "Fylo Landing Page",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Huddle Landing Page with Curved Sections",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Huddle Landing Page",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Intro Component With Signup Form",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Loopstudios Landing Page",
+    // url:""
+  },
+  {
+    id: uuidv4(),
+    title: "Manage Landing Page",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Project Tracking Landing Page",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Social Media Dashboard With Theme Switcher",
+    // url: ""
+  },
+  {
+    id: uuidv4(),
+    title: "Social Proof Section",
+    // url: ""
+  },
+]
